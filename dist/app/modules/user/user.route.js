fdd2f4ff00c7be5ee9aa1d4ba8cbafc0ace7ba21
Md Mehedi Hasan <76892775+mehedi891@users.noreply.github.com>
2024-12-21 18:24:01 +0600

    init

--- dist/app/modules/user/user.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// import express from "express"
-// import { userController } from "./user.controller";
-// import validateRequest from "../../middlewares/validateRequest";
-// import { userValidations } from "./user.validation";
-// const router = express.Router();
-// router.post('/register',validateRequest(userValidations.createUserValidationSchema),userController.createUser);
-// export const userRoutes = router;
